3c0a624b4daed17d55faeb9772f2375977868e25
Nataliia Lyfar <lifarnatala@gmail.com>
2022-01-13 00:00:17 +0200

    added general settings

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './sass/main.scss';
+
+import './js/mobile-menu';
+import './js/modal-order';
+import './js/modal-about';
+import './js/slick';
+import './js/map-modal';
+import './js/franchize-modal';
